f6ec939fc13eb4c42e45234b239a0c87662e27ce
Liudmila <to_shagalova_lu@mail.ru>
2022-09-22 15:59:53 +0500

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int count = 3;
+int[] array = new int[count];
+int index = 0;
+
+Console.WriteLine();
+while(index<count)
+{
+    array[index]=new Random().Next(1,10);
+    Console.Write(array[index]+ " ");
+    index++;
+}
+Console.WriteLine(" - трехзначное число");
+Console.Write("Вторая цифра трехзначного числа = ");
+Console.Write(array[1]);
---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿int count = new Random().Next(2, 4);
+int[] array = new int[count];
+int index = 0;
+Console.Write("Count = ");
+Console.WriteLine(count);
+Console.Write("Дано число: ");
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 10);
+    Console.Write(array[index] + " ");
+    index += 1;
+}
+
+index = 0;
+bool element = false;
+
+if (array[2] >= 0)
+{
+    element = true;
+}
+if (element)
+{
+    Console.Write("\nТретья цифра заданного числа = ");
+    Console.WriteLine(array[2]);
+}
+else
+{
+    Console.WriteLine("Такого числа нет");
+}
