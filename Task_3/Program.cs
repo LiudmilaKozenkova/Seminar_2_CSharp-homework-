cac5ae52cc8a78f08ca36ca1e3dddf8d60e5300d
Liudmila <to_shagalova_lu@mail.ru>
2022-09-23 16:00:38 +0500

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = new Random().Next(1,10);
+int find = 6;
+int find2 = 7;
+Console.Write("Случайное число = ");
+Console.Write(n);
+Console.WriteLine();
+
+if(n==find)
+{
+    Console.WriteLine("ДА");
+}
+if(n==find2)
+{
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ");
+}
